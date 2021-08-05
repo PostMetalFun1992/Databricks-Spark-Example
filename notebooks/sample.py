a01d57c985d98b514958f689b58f5ad66f1264c7
Kirill_Kabanov@epam.com <Kirill_Kabanov@epam.com>
2021-08-05 11:10:28 +0000

    Parsing config via configparser

--- notebooks/sample.py
+++ notebooks/sample.py
@@ -6,7 +6,18 @@ secret = dbutils.secrets.get(scope="abfs-access", key="storage-creds")
 config = configparser.ConfigParser()
 config.read_string(secret)
 
-print(config.sections())
+class AZStorage:
+    IN_STORAGE_ACCOUNT = config["INPUT"]["AZ_STORAGE_ACCOUNT"]
+    IN_CONTAINER = config["INPUT"]["AZ_CONTAINER"]
+    IN_CLIENT_ID = config["INPUT"]["AZ_CLIENT_ID"]
+    IN_CLIENT_SECRET = config["INPUT"]["AZ_CLIENT_SECRET"]
+    IN_CLIENT_ENDPOINT = config["INPUT"]["AZ_CLIENT_ENDPOINT"]
+
+    OUT_STORAGE_ACCOUNT = config["OUTPUT"]["AZ_STORAGE_ACCOUNT"]
+    OUT_CONTAINER = config["OUTPUT"]["AZ_CONTAINER"]
+    OUT_CLIENT_ID = config["OUTPUT"]["AZ_CLIENT_ID"]
+    OUT_CLIENT_SECRET = config["OUTPUT"]["AZ_CLIENT_SECRET"]
+    OUT_CLIENT_ENDPOINT = config["OUTPUT"]["AZ_CLIENT_ENDPOINT"]
 
 # COMMAND ----------
 
